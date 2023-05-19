1dc1231bc3dbf1e10483c24107bb8f80d8d14f6e
ecomeel <ecomeel@gmail.com>
2023-05-19 23:17:12 +0300

    refactoring code

--- js/code.js
+++ js/code.js
@@ -1,43 +1,76 @@
 const LIMIT = 10000;
+const CURRENCY = "руб.";
+const STATUS_BAD = "Badly";
+const STATUS_BAD_CLASSNAME = "status_red";
+
+const inputNode = document.getElementById("input");
+const buttonNode = document.getElementById("add-btn");
+const historyNode = document.getElementById("history");
+const sumNode = document.getElementById("sum");
+const limitNode = document.getElementById("limit");
+const statusNode = document.getElementById("status");
 
 const expenses = [];
 
-const inputNode = document.getElementById('input');
-const buttonNode = document.getElementById('add-btn');
-const historyNode = document.getElementById('history');
-const totalNode = document.getElementById('sum');
-const limitNode = document.getElementById('limit');
-const statusNode = document.getElementById('status');
+init(expenses);
 
-limitNode.innerText = LIMIT;
+buttonNode.addEventListener("click", function () {
+  const expense = getExpenseFromUser();
+  if (!expense) {
+    return
+  }
+  trackExpense(expense);
+  render(expenses);
+});
 
-buttonNode.addEventListener('click', function(){
-    // get input value
-    if (inputNode.value == '' || inputNode.value == NaN || inputNode.value == null) return;
-
-    const expense = parseInt(inputNode.value);
-    inputNode.value = '';
-
-    // Save value 
-    expenses.push(expense);
-
-    // Expenses history
-    let expensesListHTML = '';
-    expenses.forEach(el => {
-        expensesListHTML += `<li>${el} руб</li>`;
-    });
-    const html = `<ol>${expensesListHTML}</ol>`
-    historyNode.innerHTML = html;
-
-    // Expenses Summ
-    let sum = 0;
-    expenses.forEach(el => {
-        sum += el;
-    });
-    totalNode.innerText = sum
-
-    // Comparison with the limit and status output
-    if (sum > LIMIT) {
-        statusNode.innerText = 'Badly';
-    }
-})
+function init(expenses) {
+  limitNode.innerText = LIMIT;
+  sumNode.innerText = calculateExpenses(expenses);
+}
+function trackExpense(expense) {
+  expenses.push(expense);
+}
+function getExpenseFromUser() {
+  if (
+    inputNode.value == "" ||
+    inputNode.value == NaN ||
+    inputNode.value == null
+  )
+    return;
+  const expense = parseInt(inputNode.value);
+  clearInput();
+  return expense;
+}
+function clearInput() {
+  inputNode.value = "";
+}
+function calculateExpenses(expenses) {
+  let sum = 0;
+  expenses.forEach((el) => {
+    sum += el;
+  });
+  return sum;
+}
+function render(expenses) {
+  const sum = calculateExpenses(expenses);
+  renderHistory(expenses);
+  renderSum(sum);
+  renderStatus(sum);
+}
+function renderHistory(expenses) {
+  let expensesListHTML = "";
+  expenses.forEach((el) => {
+    expensesListHTML += `<li>${el} ${CURRENCY}</li>`;
+  });
+  const html = `<ol>${expensesListHTML}</ol>`;
+  historyNode.innerHTML = html;
+}
+function renderSum(sum) {
+  sumNode.innerText = sum;
+}
+function renderStatus(sum) {
+  if (sum > LIMIT) {
+    statusNode.innerText = STATUS_BAD;
+    statusNode.classList.add(STATUS_BAD_CLASSNAME);
+  }
+}
