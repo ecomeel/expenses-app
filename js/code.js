18ee03d027c765d0de6b4043d9d89dc34868304b
ecomeel <ecomeel@gmail.com>
2023-05-20 12:24:47 +0300

    done js

--- js/code.js
+++ js/code.js
@@ -1,76 +1,79 @@
-// const LIMIT = 10000;
-// const CURRENCY = "руб.";
-// const STATUS_BAD = "Badly";
-// const STATUS_BAD_CLASSNAME = "status_red";
+const STATUS_IN_LIMIT = "все хорошо";
+const STATUS_OUT_OF_LIMIT = "все плохо";
 
-// const inputNode = document.getElementById("input");
-// const buttonNode = document.getElementById("add-btn");
-// const historyNode = document.getElementById("history");
-// const sumNode = document.getElementById("sum");
-// const limitNode = document.getElementById("limit");
-// const statusNode = document.getElementById("status");
+const inputNode = document.getElementById('expensesInput');
+const addButtonNode = document.getElementById('addButton');
+const clearButtonNode = document.getElementById('clearButton');
+const limitNode = document.getElementById('limitValue');
+const totalValueNode = document.getElementById('totalValue');
+const statusNode = document.getElementById('statusText');
+const historyList = document.getElementById('historyList');
 
-// const expenses = [];
 
-// init(expenses);
+const expenses = [];
+const limit = parseInt(limitNode.innerText);
 
-// buttonNode.addEventListener("click", function () {
-//   const expense = getExpenseFromUser();
-//   if (!expense) {
-//     return
-//   }
-//   trackExpense(expense);
-//   render(expenses);
-// });
+const getTotal = () => {
+    let sum = 0;
+    expenses.forEach((expense) => {
+        sum += expense;
+    })
+    return sum;
+}
 
-// function init(expenses) {
-//   limitNode.innerText = LIMIT;
-//   sumNode.innerText = calculateExpenses(expenses);
-// }
-// function trackExpense(expense) {
-//   expenses.push(expense);
-// }
-// function getExpenseFromUser() {
-//   if (
-//     inputNode.value == "" ||
-//     inputNode.value == NaN ||
-//     inputNode.value == null
-//   )
-//     return;
-//   const expense = parseInt(inputNode.value);
-//   clearInput();
-//   return expense;
-// }
-// function clearInput() {
-//   inputNode.value = "";
-// }
-// function calculateExpenses(expenses) {
-//   let sum = 0;
-//   expenses.forEach((el) => {
-//     sum += el;
-//   });
-//   return sum;
-// }
-// function render(expenses) {
-//   const sum = calculateExpenses(expenses);
-//   renderHistory(expenses);
-//   renderSum(sum);
-//   renderStatus(sum);
-// }
-// function renderHistory(expenses) {
-//   let expensesListHTML = "";
-//   expenses.forEach((el) => {
-//     expensesListHTML += `<li>${el} ${CURRENCY}</li>`;
-//   });
-//   const html = `<ol>${expensesListHTML}</ol>`;
-//   historyNode.innerHTML = html;
-// }
-// function renderSum(sum) {
-//   sumNode.innerText = sum;
-// }
-// function renderStatus(sum) {
-//   if (sum > LIMIT) {
-//     statusNode.innerText = STATUS_BAD;
-//     statusNode.classList.add(STATUS_BAD_CLASSNAME);
-//   }
-// }
+const renderStatus = () => {
+    const total = getTotal(expenses);
+    totalValueNode.innerText = total;
+
+    if (total <= limit) {
+        statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.className = "stats__statusText_positive";
+    } else {
+        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.className = "stats__statusText_negative";
+    }
+}
+
+const renderHistory = () => {
+    historyList.innerHTML = "";
+    expenses.forEach(expense => {
+        const historyItem = document.createElement("li");
+        historyItem.className = "rub";
+        historyItem.innerText = expense;
+
+        historyList.appendChild(historyItem);
+    });
+}
+
+const render = () => {
+    renderStatus();
+    renderHistory();
+}
+
+const getExpenseFromUser = () => parseInt(inputNode.value);
+
+const clearInput = () => {
+    inputNode.value = '';
+}
+
+function addButtonHandler() {
+    const expense = getExpenseFromUser();
+    if (!expense) {
+        return
+    }
+
+    expenses.push(expense);
+
+    console.log(expenses);
+
+    render();
+    clearInput();
+}
+
+const clearButtonHandler = () => {
+    expenses = [];
+    render();
+}
+
+addButtonNode.addEventListener('click', addButtonHandler);
+clearButtonNode.addEventListener('click', clearButtonHandler);
